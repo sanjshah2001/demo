e4a2b94aee047eaadd32f390543055a3321312d4
sanjshah2001 <sanj_shah2001@yahoo.com>
2018-02-07 20:15:09 +0000

    iii

--- cre_ins.py
+++ cre_ins.py
@@ -2,7 +2,7 @@ import boto3
 ec2 = boto3.resource('ec2')
 instance = ec2.create_instances(
     ImageId='ami-9afbd6ff',
-    region_name='us-east-2',
+    SubnetId='subnet-4537fc08',
     MinCount=1,
     MaxCount=1,
     InstanceType='t2.micro')
