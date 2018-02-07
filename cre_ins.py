3c12460474498d0e12d106831bf9b757766bc4de
sanjshah2001 <sanj_shah2001@yahoo.com>
2018-02-07 19:51:23 +0000

    added

---
+++ cre_ins.py
@@ -0,0 +1,8 @@
+import boto3
+ec2 = boto3.resource('ec2')
+instance = ec2.create_instances(
+    ImageId='ami-9afbd6ff',
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro')
+print instance[0].id
