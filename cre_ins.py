2e8681c709216279395b1277f5357a82b4172815
sanjshah2001 <sanj_shah2001@yahoo.com>
2018-02-07 19:57:37 +0000

    sssd

--- cre_ins.py
+++ cre_ins.py
@@ -2,7 +2,7 @@ import boto3
 ec2 = boto3.resource('ec2')
 instance = ec2.create_instances(
     ImageId='ami-9afbd6ff',
-    region=us-east-2
+    region=us-east-2,
     MinCount=1,
     MaxCount=1,
     InstanceType='t2.micro')
